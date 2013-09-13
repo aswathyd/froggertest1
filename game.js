63b1a5f80d374a8d6ff4d0ec8c19cff71640096f
aswathyd <aswathy.dinesh@tufts.edu>
2013-09-13 11:33:43 -0400

    Update game.js

--- game.js
+++ game.js
@@ -7,11 +7,22 @@ function runRegular() {update();drawGame();drawFrog(frogger);}
 function runEndGame() {drawGame();
 deadFrog = {x: frogger.x,y: frogger.y}drawDeadFrog();clickOn = true;time = 0;
 if (score > highScore) {highScore = score;localStorage["highScore"] = highScore;isNewHighScore = true;}drawGameOver();}
-function update() {updateMovingObjects(vehicles);updateMovingObjects(logs);updateFly();
-if (isHome()) {updateHome();} 
-else {if (isCollisionDeath()) {updateCollisionDeath();} 
-else if (isUpArrow) {score += 10;isUpArrow = false;}}time--;}
-
+function update() {
+	updateMovingObjects(vehicles);
+	updateMovingObjects(logs);
+	updateFly();
+	if (isHome()) {
+		updateHome();
+	} else {
+		if (isCollisionDeath()) {
+			updateCollisionDeath();
+		} else if (isUpArrow) {
+			score += 10;
+			isUpArrow = false;
+		}
+	}
+	time--;
+}
 // Adjusts the positions of objects in an array
 function updateMovingObjects(objectArray) {
 	for (i = 0; i < objectArray.length; i++) {
