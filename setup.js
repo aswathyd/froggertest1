7d0b831172daa404c49f2c98c6a0147608558d8d
aswathyd <aswathy.dinesh@tufts.edu>
2013-09-13 12:34:58 -0400

    Update setup.js

--- setup.js
+++ setup.js
@@ -1,12 +1,5 @@
-var sprites, deadFrogSprite, canvas, ctx;
-var width = 399, height = 565;
-var timeInterval, laneSize = 35, colSize = 42;
-var score, highScore, isNewHighScore;
-var seconds, time, level;
-var numLives, gameOver, numHome;
-var frogger, deadFrog, vehicles, logs, fly;
-var inlets, badlands, frogsHome;
-var movePause, deathPause, isUpArrow, clickOn;
+var sprites, deadFrogSprite, canvas, ctx;var width = 399, height = 565;var timeInterval, laneSize = 35, colSize = 42;var score, highScore, isNewHighScore;
+var seconds, time, level;var numLives, gameOver, numHome;var frogger, deadFrog, vehicles, logs, fly;var inlets, badlands, frogsHome;var movePause, deathPause, isUpArrow, clickOn;
 var directions = {
 	left: "left",
 	up: "up",
