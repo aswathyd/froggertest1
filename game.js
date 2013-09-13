624d27781fdf5493328171df6a599839859b3621
aswathyd <aswathy.dinesh@tufts.edu>
2013-09-13 11:18:08 -0400

    Update game.js

--- game.js
+++ game.js
@@ -4,22 +4,8 @@ setInterval(function() {if (time > 0) {if (deathPause > 0) {runDeath();
 function runDeath() {drawGame();drawDeadFrog();drawDeadFrogMsg();deathPause--;}
 function runMove() {update();drawGame();drawMovingFrog();movePause--;}
 function runRegular() {update();drawGame();drawFrog(frogger);}
-function runEndGame() {
-	drawGame();
-	deadFrog = {
-		x: frogger.x,
-		y: frogger.y
-	}
-	drawDeadFrog();
-	clickOn = true;		// allows user to play again
-	time = 0;
-	if (score > highScore) {
-		highScore = score;
-		localStorage["highScore"] = highScore;
-		isNewHighScore = true;
-	}
-	drawGameOver();
-}
+function runEndGame() {drawGame();deadFrog = {x: frogger.x,y: frogger.y}drawDeadFrog();clickOn = true;time = 0;
+if (score > highScore) {highScore = score;localStorage["highScore"] = highScore;isNewHighScore = true;}drawGameOver();}
 
 // Updates the coordinates of objects that may move and then checks for events such as the frog reaching an inlet
 function update() {
